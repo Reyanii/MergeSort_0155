740b56f23d8bc44bd8599d21c55b84fadfe6fe2b
Reyanii <indriyaniv9@gmail.com>
2025-04-17 12:29:24 +0800

    menambahkan fungsi

--- MegaShort_155.cpp
+++ MegaShort_155.cpp
@@ -42,4 +42,8 @@ void mergeSort(int low, int high)
     }
 
     int mid = (low + high) / 2; // step 2
+
+    // fungsi rekursi - memanggil diri sendiri
+    mergeSort(low, mid); // step 3.a
+    mergeSort(mid + 1, high); // step 3.b
 }
