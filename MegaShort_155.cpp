33902ae4269f755635850e348a87b044bc5949ec
Reyanii <indriyaniv9@gmail.com>
2025-04-17 12:18:43 +0800

    input

--- MegaShort_155.cpp
+++ MegaShort_155.cpp
@@ -5,3 +5,30 @@ using namespace std;
 int arr[20], B[20];
 // nis array input size
 int n;
+
+void input()
+{
+    while (true)
+    {
+        cout << "Masukan Panjang element array : ";
+        cin >> n;
+
+        if (n <= 20)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\nMaksimal panjang array adalah 20";
+        }
+    }
+    cout << "\n-------------------------" << endl;
+    cout << "\nInputkan Isi elemnt array" << endl;
+    cout << "\n-------------------------" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Array index ke- " << i << " : ";
+        cin >> arr[i];
+    }
+}
